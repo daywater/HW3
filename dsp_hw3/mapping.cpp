5849b4f1a8842dc6b69b715e162f57a91bb91330
daywater <46214716+daywater@users.noreply.github.com>
2019-12-05 19:40:32 +0800

    Add files via upload

--- dsp_hw3/mapping.cpp
+++ dsp_hw3/mapping.cpp
@@ -1,1 +1,21 @@
-#include<fstream>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main(int argc,char **argv){
+
+        char* in=argv[1];
+        char* out=argv[2];
+        unordered_map<string,vector<string>> zb_map;
+
+        cout<<in<<endl;
+        cout<<out<<endl;
+
+        return 0;
+
+}
