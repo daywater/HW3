58de806df9e0fe8d283a67b307338d19d0b44c88
daywater <46214716+daywater@users.noreply.github.com>
2019-12-05 21:34:49 +0800

    Add files via upload

--- dsp_hw3/mapping.cpp
+++ dsp_hw3/mapping.cpp
@@ -12,9 +12,13 @@ int main(int argc,char **argv){
         char* in_path=argv[1];
         char* out_path=argv[2];
         unordered_map<string,vector<string>> zb_map;
-        ifstream infile
-        cout<<in<<endl;
-        cout<<out<<endl;
+        ifstream infile(in_path);
+        string line;
+        while (getline(infile,line)){
+          cout<<line<<endl;
+        }
+        cout<<in_path<<endl;
+        cout<<out_path<<endl;
 
         return 0;
 
