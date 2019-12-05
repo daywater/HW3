c766656af9f3bb44f737b2b03a59cf57509327e0
daywater <46214716+daywater@users.noreply.github.com>
2019-12-05 22:58:41 +0800

    Add files via upload

--- dsp_hw3/mapping.cpp
+++ dsp_hw3/mapping.cpp
@@ -4,67 +4,60 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
-#ifndef CHAR_LEN
-#define CHAR_LEN 2
-#endif
 
 using namespace std;
-int main(int argc, char* argv[]){
-	char* fin = argv[1];
-	char* fout = argv[2];
-	unordered_map < string, vector<string> > M;
-	string iline;
-	ifstream infile(fin);//Big5-ZhuYin.map
-	while(getline(infile, iline)){
-		vector<string> tmp;
-		string wd="", crt="";
-		int  i = -1;
-		while(iline[++i]!=' ' && iline.begin()+i < iline.end())
-			wd.push_back(iline[i]);
-		tmp.push_back(wd);
-		M[wd] = tmp;
-		while(iline.begin()+i < iline.end()){
-			if(iline[i]=='/'){
-				crt = crt.substr(0,CHAR_LEN);
-				bool NotExist = (M.find(crt)== M.end());
-				if(NotExist) M[crt] = tmp;
-				else{
-					if(M[crt].back()!=wd) M[crt].push_back(wd);
-				}
-				crt.clear();
-			}
-			if(iline[++i]!=' ') crt.push_back(iline[i]);
-		}
-		crt = crt.substr(0,CHAR_LEN);
-		bool NotExist = (M.find(crt)== M.end());
-		if(NotExist) M[crt] = tmp;
-		else{
-			if(M[crt].back()!=wd) M[crt].push_back(wd);
-		}
-		tmp.pop_back();
-	}
-	infile.close();
-	#if 0 //terminal debug
-	for(const auto &i : M){
-		cout << i.first << '\t';
-		for(string j : i.second)
-			cout << j << ' ';
-		cout << '\n';
-	}//output Map
-	#endif
-	#if 1
-	char enter[1];
-	enter[0] = '\n';
-	ofstream outfile(fout);
-	for(const auto &i : M){
-		outfile << i.first <<'\t';
-		for(string j : i.second)
-			outfile << j << ' ';
-		long pos = outfile.tellp();
-    	outfile.seekp(pos - 1); 
-		outfile.write(enter,1);
-	}
-	outfile.close();
-	#endif
-	return 0;
+
+int main(int argc,char **argv){
+
+        char* in_path=argv[1];
+        char* out_path=argv[2];
+        unordered_map<string,vector<string>> zb_map;
+        unordered_map<int,int> _map;
+        ifstream infile(in_path);
+        string line;
+
+        while (getline(infile,line)){
+          vector<string> tmp;
+          string w,c="","";
+
+          wd.push_back(line[0]);
+          wd.push_back(line[1]);
+          tmp.push_back(wd);
+
+          int i = -1;
+          while(iline.begin()+i < iline.end()){
+            i++;
+      			if(line[i]=='/' || line[i]==' '){
+      				c = line.substr(i+1,i+3);
+      				bool NotExist = (zb_map.find(c)== zb_map.end());
+      				if(NotExist) zb_map[c] = tmp;
+      				else{
+      					if(zb_map[c].back()!=w) zb_map[c].push_back(w);
+      				}
+
+      			}
+
+      		}
+          tmp.pop_back();
+
+        }
+        char enter[1];
+        enter[0] = '\n';
+        ofstream out_path(fout);
+        for(const auto &i : zb_map){
+          out_path << i.first <<'\t';
+          for(string j : i.second)
+            out_path << j << ' ';
+          long pos = out_path.tellp();
+            out_path.seekp(pos - 1);
+          out_path.write(enter,1);
+        }
+        out_path.close();
+
+
+        cout<<in_path<<endl;
+        cout<<out_path<<endl;
+
+        return 0;
+
 }
