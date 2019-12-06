6314bdfcb87fe69f62e99c944d625b4daad0b9d7
daywater <46214716+daywater@users.noreply.github.com>
2019-12-06 11:52:52 +0800

    Add files via upload

--- dsp_hw3/mydisambig.cpp
+++ dsp_hw3/mydisambig.cpp
@@ -1,1 +1,22 @@
-#include<fstream>
+#include <stdio.h>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <cstdlib>
+#include <~/srilm-1.5.10/include>
+
+
+int main(int argc, char** argv){
+  char *seg_path=argv[1];
+  char *map_path=argv[2];
+  char *mod_path=argv[3];
+  char *out_path=argv[4];
+  Vocab c;
+
+
+
+  return 0;
+}
